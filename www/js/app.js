1e0daa89bd63a6510f1d941923f3da93ebcace73
Karuna <imkaruna@gmail.com>
2017-09-14 22:22:56 -0400

    Created tasks in lists and added to view

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,15 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
-
+angular.module('todo', ['ionic'])
+.controller('TodoCtrl', function($scope) {
+  $scope.tasks = [
+    { title: 'Collect coins' },
+    { title: 'Eat mushrooms' },
+    { title: 'Get high enough to grab the flag' },
+    { title: 'Find the Princess' }
+  ];
+})
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
